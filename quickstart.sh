71fa84ec0daa8f2e0e20f828107f1b09d9c83d56
Conrad Sachweh <conrad@csachweh.de>
2018-04-20 10:50:24 +0200

    after reboot quickstart of some programs, better not automatically so with htis script should work

---
+++ quickstart.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PROGRAMS=( evolution telegram-desktop signal-desktop rambox )
+
+for p in "${PROGRAMS[@]}"
+do
+    if pgrep -f "$p" >/dev/null 2>&1 ; then
+	echo "$p already running"
+    else
+	$p >/dev/null 2>&1 & disown
+    fi
+done
