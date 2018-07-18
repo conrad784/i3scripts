c8553027fa81b0488b83edf26b6bf77356b7632a
Conrad Sachweh <conrad@csachweh.de>
2018-07-18 10:23:08 +0200

    storing workspaces now with i3ipc
    attention, only working with versions > 1.5.1
    https://github.com/acrisci/i3ipc-python/issues/89

--- i3plug.py
+++ i3plug.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
-import os
-import i3
+import i3ipc
 import sys
 import pickle
 
-
 PATH = "/home/conrad/.i3_workspace_mapping"
+
 
 def showHelp():
     print(sys.argv[0] + " [save|restore]")
@@ -16,20 +15,22 @@ if __name__ == '__main__':
         showHelp()
         sys.exit(1)
 
+    i3 = i3ipc.Connection()
+
     if sys.argv[1] == 'save':
         pickle.dump(i3.get_workspaces(), open(PATH, "wb"))
     elif sys.argv[1] == 'restore':
         try:
             workspace_mapping = pickle.load(open(PATH, "rb"))
-        except Exception:
+        except FileNotFoundError:
             print("Can't find existing mappings...")
             sys.exit(1)
 
         for workspace in workspace_mapping:
-            i3.msg('command', 'workspace %s' % workspace['name'])
-            i3.msg('command', 'move workspace to output %s' % workspace['output'])
+            i3.command(f"workspace {workspace['name']}")
+            i3.command(f"move workspace to output {workspace['output']}")
         for workspace in filter(lambda w: w['visible'], workspace_mapping):
-            i3.msg('command', 'workspace %s' % workspace['name'])
+            i3.command(f"workspace {workspace['name']}")
     else:
         showHelp()
         sys.exit(1)
